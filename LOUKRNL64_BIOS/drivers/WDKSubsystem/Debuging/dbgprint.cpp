e23dc4f486ffff9f586b2007a2150f04d55734fc
AlienMaster815 <tgrenier.815@gmail.com>
2023-11-19 23:59:23 -0500

    Driver Update

---
+++ LOUKRNL64_BIOS/drivers/WDKSubsystem/Debuging/dbgprint.cpp
@@ -0,0 +1,6 @@
+#include <WDKSubsystem/Debuging/Debuging.h>
+
+int64_t DbgPrint(){
+    
+    return 0;
+}
---
+++ LOUKRNL64_BIOS/include/WDKSubsystem/Debuging/Debuging.h
@@ -0,0 +1,19 @@
+/*
+-- Tyler Grenier 11/19/23 11:33
+-- Not Sorry For Typos Its Almost Midnight Get
+-- Over It...
+*/
+
+
+#ifndef _DEBUGING_H
+#define _DEBUGING_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+
+extern "C" int64_t DbgPrint();
+
+
+
+#endif
