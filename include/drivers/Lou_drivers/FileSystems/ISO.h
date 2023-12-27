1ad149b29478d2285d9989949a5442a50a75288b
AlienMaster815 <tgrenier.815@gmail.com>
2023-12-27 12:53:22 -0500

    First Update In a While
    Created an algorithm for the iso filesystem handler to be able to interpret the data from the ATAPI to the Volume Descriptor Table System

--- include/drivers/Lou_drivers/FileSystems/ISO.h
+++ include/drivers/Lou_drivers/FileSystems/ISO.h
@@ -53,7 +53,7 @@ typedef struct _sint32_LSB_MSB{
 
 typedef struct _VolumeDescriptor{
     int8 Type;
-    strA Identifier;
+    strA Identifier[5];
     int8 Version;
     int8_t Data[2041];
 }VolumeDescriptor, *PVolumeDescriptor;
