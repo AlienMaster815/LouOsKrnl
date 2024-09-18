3d952192e2ab9577400d4dda6a49f243949cdee1
AlienMaster815 <tgrenier.815@gmail.com>
2024-09-18 17:22:30 -0400

    Pre AmdGpu Merge

---
+++ drivers/gpu/AMD/Radeon/R100.cpp
@@ -0,0 +1,19 @@
+#include "ObjectID.h"
+#include "atom-bits.h"
+#include "atom.h"
+#include "atom-names.h"
+#include "atombios.h"
+#include "avivod.h"
+
+#include <LouDDK.h>
+#include <NtAPI.h>
+
+
+LOUSTATUS InitializeAmdR100Gpu(
+    P_PCI_DEVICE_OBJECT PDEV
+){
+
+
+
+    return STATUS_SUCCESS;
+}
