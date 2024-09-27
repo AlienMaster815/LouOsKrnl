1bd7f148ff277c12132aaf8914a8cbdcccfa71cf
AlienMaster815 <tgrenier.815@gmail.com>
2024-09-27 16:49:19 -0400

    end of day push

--- kernel/ProgramLoading/SystemContextCreation.c
+++ kernel/ProgramLoading/SystemContextCreation.c
@@ -1,15 +1,21 @@
 //This file is for createing a system compatible enviornment
 #include <LouAPI.h>
 
+static mutex_t SystemContextMutex;
+
 bool 
 LouKeRunSystemPciScanContext(
     uintptr_t PScanFunction, 
     uintptr_t PDEV, 
     uintptr_t PDriverObject, 
-    uintptr_t PRegistryString    
+    uintptr_t PRegistryString,
+    uint64_t StackSize    
 ){
+    MutexLock(&SystemContextMutex);
+    void* SystemStack = LouMallocEx(StackSize, 16);
 
-    
 
+    LouFree(SystemStack);
+    MutexUnlock(&SystemContextMutex);
     return false;
 }
